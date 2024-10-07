e3469aea8d95038b5fd881ee0f394c1264e0d890
Felipe <pipe.ma2310@gmail.com>
2024-10-07 14:30:42 -0600

    carrusel horizontal funciona

--- tailtales/src/Landing.js
+++ tailtales/src/Landing.js
@@ -1,10 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './styles/Landing.css';
+import imagen1 from './img/landing-img-1.png';
+import imagen2 from './img/landing-img-2.png';
+import imagen3 from './img/landing-img-3.png';
+import imagen4 from './img/landing-img-4.png';
 
 function App() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const images = [imagen1, imagen2, imagen3, imagen4];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActiveIndex((activeIndex + 1) % images.length);
+    }, 5000); // Cambia la imagen cada 3 segundos
+
+    return () => clearInterval(intervalId);
+  }, [activeIndex, images]);
   return (
     <div className="App">
-      <img src={logo} className="logo" alt="logo" />
+      <div className="carrusel">
+      <div
+        className="carrusel-inner"
+        style={{
+          width: `${images.length * 100}vw`,
+          transform: `translateX(-${activeIndex * 100}vw)`, // Desplaza el carrusel hacia la izquierda
+        }}
+      >
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`carrusel-item ${activeIndex === index ? 'active' : ''}`}
+            style={{
+              backgroundImage: `url(${image})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              height: '100vh',
+              width: '100vw',
+              position: 'absolute',
+              top: 0,
+              left: `${index * 100}vw`, // Desplaza las imágenes horizontalmente
+              opacity: activeIndex === index ? 1 : 0,
+            transition: 'opacity 0.5s',
+            }}
+          />
+        ))}
+      </div>
+    </div>
+      <a href='./Landing.js'><img src={logo} className="logo" alt="logo" /></a>
       <h1 className='titulo-bienvenida'>Bienvenid@ a TailTales</h1>
       <p className='texto'>Lorem ipsum dolor sit amet,<br/> 
       consectetur adipiscing elit. Proin <br/>
