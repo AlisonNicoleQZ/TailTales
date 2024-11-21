d0485f9b9c0e35602b8c0e2d6e4cb71ec6675ca0
nxttian <sebasespi06@gmail.com>
2024-11-21 05:29:15 -0600

    JSX actualizado, navegacion añadida de Landing a LoginRegister y CSS de Login

--- tailtales/src/login/LoginRegister.jsx
+++ tailtales/src/login/LoginRegister.jsx
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signInWithPopup,
-  GoogleAuthProvider
-} from 'firebase/auth';
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signInWithPopup, GoogleAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, collection, query, where, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import logo from '../img/logo.svg';
+import styles from './LoginRegister.module.css';
 
 // Configuración de Firebase
 const firebaseConfig = {
@@ -26,28 +20,29 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
 const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
-  'client_id': "365635220712-eacs2040u28q0vb4rkkcrg28g1lnr14e.apps.googleusercontent.com"
-});
+provider.setCustomParameters({ 'prompt': 'select_account' });
 
-const LoginRegister = () => {
+export const LoginRegister = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
   const isRegistering = queryParams.get('register') === 'true';
   const [isLogin, setIsLogin] = useState(!isRegistering);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState("");
   const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
   const [species, setSpecies] = useState('');
   const [breed, setBreed] = useState('');
   const [age, setAge] = useState(0);
+  const [ageFormat, setAgeFormat] = useState("years");
+  const [ageError, setAgeError] = useState(false);
   const [profilePic, setProfilePic] = useState('');
   const [bio, setBio] = useState('');
   const [locationField, setLocationField] = useState('');
   const [privacySettings, setPrivacySettings] = useState('1');
-  const [ageError, setAgeError] = useState(false);
 
   const breeds = {
     Perro: [
@@ -58,9 +53,9 @@ const LoginRegister = () => {
     ],
     Gato: [
       'Maine Coon', 'Persa', 'Siames', 'Siberiano', 'Bengala',
-      'Sphynx', 'Ragdoll', 'British Shorthair', 'Scottish Fold', 'Abyssinian',
+      'Sphynx', 'Ragdoll', 'Calicó', 'British Shorthair', 'Scottish Fold', 'Abyssinian',
       'Birmano', 'Burmese', 'Oriental', 'Somalí', 'Cornish Rex',
-      'Angora', 'Siberiano', 'Toyger', 'Chausie', 'Manx'
+      'Angora', 'Toyger', 'Chausie', 'Manx'
     ]
   };
 
@@ -79,74 +74,81 @@ const LoginRegister = () => {
       setAgeError(false);
     }
   }, [age]);
+  
 
+  const handleAgeFormatChange = (event) => {
+    setAgeFormat(event.target.value);
+  };
+
+  // Función para limpiar el formulario de inicio de sesión
   const resetLoginForm = () => {
     setEmail('');
     setPassword('');
   };
 
-  const handleLoginSubmit = (event) => {
+  // Función para verificar el estado de la cuenta y decidir si redirigir al perfil
+  const checkAccountStatus = async (uid) => {
+    const userDoc = await getDoc(doc(db, "users", uid));
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      if (userData.status === false) {
+        const confirmation = confirm("La cuenta actualmente está deshabilitada. ¿Desea habilitarla e ingresar?");
+        if (confirmation) {
+          await updateDoc(doc(db, "users", uid), { status: true });
+          alert("Cuenta habilitada exitosamente.");
+          navigate('/perfil'); // Redirigir al perfil
+        } else {
+          await signOut(auth);
+          alert("Ha cancelado el acceso.");
+        }
+      } else {
+        navigate('/perfil'); // Redirigir al perfil si está habilitada
+      }
+    } else {
+      alert("No se encontró el perfil del usuario.");
+    }
+  };
+
+  // Manejar inicio de sesión con correo electrónico y contraseña
+  const handleLoginSubmit = async (event) => {
     event.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        alert('Inicio de sesión exitoso');
-        window.location.href = '/home';
-      })
-      .catch((error) => {
-        alert('Error al iniciar sesión: ' + error.message);
-        resetLoginForm(); // Limpiar el formulario después de un error
-      });
+    .then(async (userCredential) => {
+      const user = userCredential.user;
+      await checkAccountStatus(user.uid);
+    })
+    .catch((error) => {
+      if (error.code === 'auth/user-not-found') {
+        alert('Usuario no registrado. Por favor, verifique sus credenciales.');
+      } else {
+        alert('Usuario o contraseña inválido, por favor inténtelo de nuevo');
+      }
+      resetLoginForm();
+    });
   };
 
-  const handleRegisterSubmit = (event) => {
+  // Manejar inicio de sesión con Google
+  const handleGoogleLogin = async (event) => {
     event.preventDefault();
-    if (age <= 0) return;
-
-    // Crear usuario en Firebase Authentication
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(async (userCredential) => {
-        const user = userCredential.user;
-
-        // Generar un petId aleatorio
-        const petId = Math.floor(Math.random() * 1000000);
-
-        // Crear el documento del usuario en Firestore
-        await setDoc(doc(db, "users", user.uid), {
-          petId: petId,
-          mail: email,
-          name: name,
-          species: species,
-          breed: breed,
-          age: parseInt(age),
-          profilePic: profilePic,
-          bio: bio,
-          location: locationField,
-          privacySettings: parseInt(privacySettings),
-          status: true,
-          createdAt: new Date().toLocaleString()
-        });
-        alert("Usuario registrado exitosamente y datos guardados en Firestore.");
-        Navigate('/login-register');
-      })
-      .catch((error) => {
-        console.error("Error al registrar el usuario: ", error);
-        alert("Error al registrar el usuario: " + error.message);
-      });
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      await checkAccountStatus(user.uid);
+    } catch (error) {
+      alert('Error al iniciar sesión con Google: ' + error.message);
+    }
   };
 
+  // Función para restablecer la contraseña usando hyperlink
   const handleForgotPassword = (event) => {
-    event.preventDefault(); // Evitar comportamiento por defecto del enlace
+    event.preventDefault();
     const email = prompt('Por favor, ingrese su correo electrónico:');
-    
     if (email) {
       sendPasswordResetEmail(auth, email)
-        .then(() => {
-          alert('Se ha enviado un correo para restablecer su contraseña. Verifique su bandeja de entrada.');
-        })
+        .then(() => alert('Se ha enviado un correo para restablecer su contraseña. Verifique su bandeja de entrada.'))
         .catch((error) => {
           if (error.code === 'auth/user-not-found') {
-            alert('Email no registrado en la plataforma, por favor inténtelo de nuevo');
+            alert('Email no registrado en plataforma, por favor inténtelo de nuevo');
           } else {
             alert('Ocurrió un error al intentar restablecer la contraseña: ' + error.message);
           }
@@ -156,16 +158,85 @@ const LoginRegister = () => {
     }
   };
 
-  const handleGoogleLogin = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        alert('Inicio de sesión con Google exitoso');
-        window.location.href = '/home';
-      })
-      .catch((error) => {
-        alert('Error al iniciar sesión con Google: ' + error.message);
+  // Manejar el registro de usuarios con verificación de username único
+  const handleRegisterSubmit = async (event) => {
+    event.preventDefault();
+
+    // Validación de la contraseña
+    if (password.length < 6) {
+      setPasswordError("La contraseña debe tener al menos 6 caracteres.");
+    } else {
+      setPasswordError("");
+    }
+  
+    try {
+      const usersCollection = collection(db, "users");
+      const usernameQuery = query(usersCollection, where("username", "==", username));
+      const usernameSnapshot = await getDocs(usernameQuery);
+  
+      if (!usernameSnapshot.empty) {
+        alert("El nombre de usuario ya está en uso. Por favor, elige otro.");
+        return;
+      }
+  
+      // Crear el documento del usuario en Firestore
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+  
+      // Generar un petId aleatorio
+      const petId = Math.floor(Math.random() * 1000000);
+  
+      // Crear el documento del usuario en Firestore
+      await setDoc(doc(db, "users", user.uid), {
+        petId: petId,
+        mail: email,
+        username: username,
+        name: name,
+        species: species,
+        breed: breed,
+        age: parseInt(age),
+        age_format: ageFormat,
+        profilePic: profilePic,
+        bio: bio,
+        location: locationField,
+        privacySettings: parseInt(privacySettings),
+        status: true,
+        createdAt: new Date().toLocaleString()
       });
+      alert("Usuario registrado exitosamente y datos guardados en Firestore.");
+      navigate('/login-register?register=false');
+      setIsLogin(true); // Redirigir al inicio de sesión
+    } catch (error) {
+      console.error("Error al registrar el usuario: ", error);
+      alert("Error al registrar el usuario: " + error.message);
+    }
+  };
+
+  const handleCompleteRegisterSubmit = async (event) => {
+    try {
+      const userId = auth.currentUser.uid;
+      await setDoc(doc(db, "users", userId), {
+        mail: email,
+        username: username,
+        name: name,
+        species: species,
+        breed: breed,
+        age: parseInt(age),
+        age_format: ageFormat,
+        profilePic: profilePic,
+        bio: bio,
+        location: locationField,
+        privacySettings: parseInt(privacySettings),
+        status: true,
+        createdAt: new Date().toLocaleString()
+      });
+      alert("Registro completado exitosamente.");
+      navigate('/login-register?register=false');
+      setIsLogin(true); // Redirigir al inicio de sesión
+    } catch (error) {
+      console.error("Error al completar el registro: ", error);
+      alert("Error al completar el registro: " + error.message);
+    }
   };
 
   // Manejo de la selección dinámica de razas en función de la especie
@@ -175,12 +246,13 @@ const LoginRegister = () => {
   };
 
   return (
-    <div className="container">
+    <div className={`${styles.container} ${styles.noScroll}`}>
+      <a className='anchor' href='/'><img src={logo} className={styles.logo} alt="logo" /></a>
       {isLogin ? (
-          <div id="login-container">
-            <h1>Iniciar Sesión en Taitales</h1>
-            <form id="login-form" onSubmit={handleLoginSubmit}>
-              <div className="input-group">
+          <div id="login-container" className={styles.loginContainer}>
+            <h1 className={styles.headingBox}>Iniciar sesión</h1>
+            <form id="login-form" className={styles.loginForm} onSubmit={handleLoginSubmit}>
+              <div className={styles.inputsLogin}>
                 <label htmlFor="login-email">Correo Electrónico</label>
                 <input
                   type="email"
@@ -191,7 +263,7 @@ const LoginRegister = () => {
                   required
                 />
               </div>
-              <div className="input-group">
+              <div className={styles.inputsLogin}>
                 <label htmlFor="login-password">Contraseña</label>
                 <input
                   type="password"
@@ -202,22 +274,23 @@ const LoginRegister = () => {
                   required
                 />
               </div>
-              <button type="submit" className="btn">Iniciar Sesión</button>
-            </form>
-      
-            <div className="forgot-password">
+              <button type="submit" className={styles.loginButton}>Iniciar Sesión</button>
+              <div className={styles.forgotPassword}>
               <a href="#" id="forgot-password" onClick={handleForgotPassword}>Olvidé mi contraseña</a>
             </div>
             <div className="register-link">
-              <p>¿No tienes una cuenta? <a href="#" id="go-to-register" onClick={() => setIsLogin(false)}>Regístrate aquí</a></p>
+              <p className={styles.registerText}>¿No tienes una cuenta? <a href="#" id="go-to-register" className={styles.registerLink} onClick={() => setIsLogin(false)}>Regístrate aquí</a></p>
             </div>
       
             <div className="social-login">
-              <button id="google-login" className="btn social-btn" onClick={handleGoogleLogin}>Iniciar con Google</button>
+              <button id="google-login" className={styles.googleLogin} onClick={handleGoogleLogin}>Iniciar con Google</button>
               {/* <button id="facebook-login" className="btn social-btn">Iniciar con Facebook</button> */}
             </div>
+            </form>
           </div>
+
       ) : (
+
         <div id="register-container">
           <h1>Regístrate en Taitales</h1>
           <form id="register-form" onSubmit={handleRegisterSubmit}>
@@ -228,6 +301,18 @@ const LoginRegister = () => {
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                placeholder="Ingresa tu correo"
+                required
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="username">Nombre de Usuario</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Ingresa tu nombre de usuario"
                 required
               />
             </div>
@@ -238,8 +323,14 @@ const LoginRegister = () => {
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                placeholder="Ingresa tu contraseña"
                 required
               />
+              {passwordError && (
+                <span style={{ color: 'red' }}>
+                  La contraseña debe tener al menos 6 caracteres
+                </span>
+              )}
             </div>
             <div className="input-group">
               <label htmlFor="name">Nombre de la Mascota</label>
@@ -248,6 +339,7 @@ const LoginRegister = () => {
                 id="name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+                placeholder="Ingresa el nombre de tu mascota"
                 required
               />
             </div>
@@ -274,7 +366,9 @@ const LoginRegister = () => {
               >
                 <option value="">Selecciona una raza</option>
                 {(breeds[species] || []).map((breedOption) => (
-                  <option key={breedOption} value={breedOption}>{breedOption}</option>
+                  <option key={breedOption} value={breedOption}>
+                    {breedOption}
+                  </option>
                 ))}
               </select>
             </div>
@@ -285,9 +379,24 @@ const LoginRegister = () => {
                 id="age"
                 value={age}
                 onChange={(e) => setAge(e.target.value)}
+                placeholder="Ingresa la edad de tu mascota"
                 required
               />
-              {ageError && <span style={{ color: 'red' }}>La edad debe ser mayor a 0</span>}
+              {ageError && (
+                <span style={{ color: 'red' }}>La edad debe ser mayor a 0</span>
+              )}
+            </div>
+            <div className="input-group">
+              <label htmlFor="age_format">Formato de Edad</label>
+              <select
+                id="age_Format"
+                value={ageFormat}
+                onChange={handleAgeFormatChange}
+                required
+              >
+                <option value="years">Años</option>
+                <option value="months">Meses</option>
+              </select>
             </div>
             <div className="input-group">
               <label htmlFor="profilePic">URL de la Foto de Perfil</label>
@@ -296,6 +405,7 @@ const LoginRegister = () => {
                 id="profilePic"
                 value={profilePic}
                 onChange={(e) => setProfilePic(e.target.value)}
+                placeholder="Ingresa la URL de la foto de perfil"
                 required
               />
             </div>
@@ -305,6 +415,7 @@ const LoginRegister = () => {
                 id="bio"
                 value={bio}
                 onChange={(e) => setBio(e.target.value)}
+                placeholder="Ingresa la biografía de tu mascota"
                 required
               />
             </div>
@@ -318,7 +429,9 @@ const LoginRegister = () => {
               >
                 <option value="">Selecciona tu país</option>
                 {countries.map((country) => (
-                  <option key={country} value={country}>{country}</option>
+                  <option key={country} value={country}>
+                    {country}
+                  </option>
                 ))}
               </select>
             </div>
@@ -334,15 +447,20 @@ const LoginRegister = () => {
                 <option value="2">Privado</option>
               </select>
             </div>
-            <button type="submit" className="btn">Registrarse</button>
+            <button type="submit" className="btn">
+              Registrarse
+            </button>
           </form>
           <div className="login-link">
-            <p>¿Ya tienes una cuenta? <a href="#" onClick={() => setIsLogin(true)}>Inicia sesión aquí</a></p>
+            <p>
+              ¿Ya tienes una cuenta?{' '}
+              <a href="#" onClick={() => setIsLogin(true)}>
+                Inicia sesión aquí
+              </a>
+            </p>
           </div>
         </div>
       )}
     </div>
   );
 };
-
-export default LoginRegister;
